c7b376b822e3fed885a77b9a4d25202b3515f684
Ypenurakar02 <30385242+Ypenurakar02@users.noreply.github.com>
2020-03-29 18:23:56 +0530

    Updates

---
+++ precache-manifest.3bc03b9020cdbc5d4b9f31809aff47a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ddcae5d7ca93431b172ef70f2126ba6",
+    "url": "/Tic-tac-toe-game-reactjs/index.html"
+  },
+  {
+    "revision": "30544f13183828714153",
+    "url": "/Tic-tac-toe-game-reactjs/static/css/main.12d7384e.chunk.css"
+  },
+  {
+    "revision": "013e98866ff66cbb31f7",
+    "url": "/Tic-tac-toe-game-reactjs/static/js/2.e5c367d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-tac-toe-game-reactjs/static/js/2.e5c367d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30544f13183828714153",
+    "url": "/Tic-tac-toe-game-reactjs/static/js/main.e007d687.chunk.js"
+  },
+  {
+    "revision": "5cfea56dc79c005a0652",
+    "url": "/Tic-tac-toe-game-reactjs/static/js/runtime-main.68885ae2.js"
+  }
+]);
